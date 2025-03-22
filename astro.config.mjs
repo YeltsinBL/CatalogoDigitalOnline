9492b86b76bbab15221663db24fcbdea39b81e91
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2025-03-22 11:43:02 -0500

    Instalación de Tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
+    plugins: [tailwindcss()],
     resolve: {
       alias: {
         "@components": "/src/components",
         "@layouts": "/src/layouts",
         "@utils": "/src/utils",
-        "@assets": "/src/assets"
+        "@assets": "/src/assets",
+        "@styles": "/src/styles"
       }
     }
   }
