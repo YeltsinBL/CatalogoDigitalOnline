22cb20c12a18637f0dd1485cc6e5945a0491167a
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2025-03-27 12:26:42 -0500

    Modificación en el Boton de Agregar al carrito

--- src/components/CardBuyButton.jsx
+++ src/components/CardBuyButton.jsx
@@ -1,18 +1,31 @@
 import { Add, Minus } from "@icons/Buy"
 import { useCartInitializer, useCartStore } from "@store/cartStore"
 
-export function CardBuyButton ({id}) {
+export function CardBuyButton ({principal= true, id, name, price}) {
   useCartInitializer()
   const { cart, addToCart, removeFromCart } = useCartStore()
 
   // Verifica si el producto ya está en el carrito
   const isInCart = cart.some((item) => item.id === id);
 
-  const handleClick = () => isInCart ? removeFromCart(id) : addToCart({ id })
+  const handleClick = () => isInCart ? removeFromCart(id) : addToCart({ id, name, price });
 
   return (
-    <button onClick={handleClick} className="card-buy-button rounded-full " >
-      {isInCart ? <Minus /> : <Add />}
-    </button>
+    <>
+    {
+      principal ? (
+        <button onClick={handleClick} className="card-buy-button rounded-full " >
+          {isInCart ? <Minus /> : <Add />}
+        </button>
+      ) : (
+        <button 
+          className={`mt-4 px-4 py-2 rounded ${isInCart ? "bg-red-500" : "bg-green-500"} text-white`} 
+          onClick={handleClick}
+        >
+          {isInCart ? "Quitar del carrito" : "Agregar al carrito"}
+        </button>
+      )
+    }
+    </>
   )
 }
