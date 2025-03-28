56e763469c6c6cefd5ea61f6b290f100da791ca6
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2025-03-28 17:47:08 -0500

    Actualizar CardBuyButton para usar un objeto de producto y cambiar el ícono de botón

--- src/components/CardBuyButton.jsx
+++ src/components/CardBuyButton.jsx
@@ -1,21 +1,25 @@
-import { Add, Minus } from "@icons/Buy"
+import { Add, MinusCircle } from "@icons/Buy"
 import { useCartInitializer, useCartStore } from "@store/cartStore"
 
-export function CardBuyButton ({principal= true, id, name, price}) {
+export function CardBuyButton ({principal= true, product}) {
   useCartInitializer()
   const { cart, addToCart, removeFromCart } = useCartStore()
 
   // Verifica si el producto ya está en el carrito
-  const isInCart = cart.some((item) => item.id === id);
+  const isInCart = cart.some((item) => item.id === product.id);
 
-  const handleClick = () => isInCart ? removeFromCart(id) : addToCart({ id, name, price });
+  const handleClick = () => isInCart ? 
+    removeFromCart(product.id) : 
+    addToCart({id:product.id, title:product.title, price:product.price, image:product.image});
 
   return (
     <>
     {
       principal ? (
         <button onClick={handleClick} className="card-buy-button rounded-full " >
-          {isInCart ? <Minus /> : <Add />}
+          {isInCart ? 
+            <MinusCircle fill={"#6b6b6b"} width={"48"} height={"48"}/> : 
+            <Add fill={"none"} width={"48"} height={"48"} />}
         </button>
       ) : (
         <button 
